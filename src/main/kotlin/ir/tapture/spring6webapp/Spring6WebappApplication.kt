eb2347e18102bd1f76a6b63b67710b8762e6dbe1
a.karamiani <aziz.karamiani@gmail.com>
2024-06-03 19:05:13 +0330

    Init

---
+++ src/main/kotlin/ir/tapture/spring6webapp/Spring6WebappApplication.kt
@@ -0,0 +1,11 @@
+package ir.tapture.spring6webapp
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Spring6WebappApplication
+
+fun main(args: Array<String>) {
+    runApplication<Spring6WebappApplication>(*args)
+}
---
+++ src/test/kotlin/ir/tapture/spring6webapp/Spring6WebappApplicationTests.kt
@@ -0,0 +1,13 @@
+package ir.tapture.spring6webapp
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class Spring6WebappApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
